f02f6a73794facf980175b806cda43b5c3360972
erikprakoso <erikprakoso1993@gmail.com>
2026-01-10 18:29:36 +0700

    fix: change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // TODO: Replace with actual domain
+  site: 'https://www.maspetelas.my.id',
   vite: {
     plugins: [tailwindcss()],
   },
